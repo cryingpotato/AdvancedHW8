75e3c0ece3660c1d2e5b8973dc5bc24fff65f5ea
cryingpotato <qq1591680@gmail.com>
2020-01-04 17:34:02 +0800

    Initial commit

---
+++ AdvancedHW8/src/main/java/com/example/advancedhw8/MyClass.java
@@ -0,0 +1,9 @@
+package com.example.advancedhw8;
+
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        IN cppBank = new IN(Integer.parseInt(scanner.next()),Integer.parseInt(scanner.next()));
+    }
+}
